2b43d44f292be3c5cfd61b97ed0f3214b6f80641
gemimarosier <rosier.gemima95@gmail.com>
2016-03-26 02:03:20 -0400

    1 commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# My very first scraper: Radio Stations
+
+### The purpose of this scraper is to get the information on radio stations on 3 major areas in Florida. I wanted to get the name of the radio station, the number of followers, the genre of music and what the station is located. 
+
+#First I had to construct the urls of the area pages of Orlando, West Palm Beach, and Miami with the function get_area_urls. Then I scraped the urls of the individual station pages using the function get_stations. With these scraped I was able to use the function get_details to get the name, number of followers, genre and location. 
+
+###I ran into a few problems: 
+
+###(1)The area pages contained all the links to the stations and a table that only showed 7 links at a time. When a user clicks to get more, the urls did not change. For a solution, I was guided construct a new urls from the HTML that gave me the all the stations!
+
+###(2)Some of the pages did not have a genre, so my script would throw an error and stop running. I was guided to insert an try/except statemnet to tell my script to write in 'None' if a genre category was not there. 
+
+###(3)When trying to store my data into a csv, I would either see the data wrapped on tags or 'None' written into all the cells. After countless trial and error, I found a solution!! For every detail I scraped, I stored the findings on a variable. Then I stored all the variables in a list radio_stations. Next, I created an empty array. Then, I  looped the items into row. Row was then written into the csv file-- with no tags attached! 
---
+++ newradio.py
@@ -0,0 +1,116 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+import time
+import csv
+
+csvfile = open("radio_station10.csv", 'w', newline='', encoding='utf-8')
+c = csv.writer(csvfile)
+c.writerow(['name', 'followers', 'genres', 'location'])
+
+
+westpalm = "radio/West-Palm-Beach-Boca-Raton-r100047/"
+orlando  = "radio/Orlando-r100038/"
+miami =    "radio/Miami-Ft-Lauderdale-Hollywood-r100012/"
+area_url = []
+stations = []
+cities = [westpalm, orlando, miami]
+radio_stations = []
+
+
+url_root = 'http://tunein.com/'
+
+def get_area_urls(cities):
+    for city in cities:
+        offset_num = 0
+        offset_str = str(offset_num)
+        while offset_num < 106:
+            # regex string "(?<=-r)(.)*(?=\/)" )  - it says, start: -r, end: /, get the middle
+            result = re.search( "(?<=-r)(.)*(?=\/)" , city )
+            city_rnum = str( result.group() )
+            # print(city_rnum) # this will print only 3 numbers, 1 per city
+            string = url_root + city + "?id=r" + city_rnum + "&page=true&filter=!p%3a!e&offset=" + offset_str
+            # instead of printing, you would save the URLs to a list
+            #print(string)
+            area_url.append(string)
+            offset_num += 7
+            offset_str = str(offset_num)
+
+get_area_urls(cities)
+
+#--------------------------------------------------------
+
+def get_stations(area_url):
+    for url in area_url:
+        new_url = url
+        html = urlopen(url)
+        bsObj = BeautifulSoup(html, "html.parser")
+        tag_list = bsObj.findAll( "a", {"class":"_tooltip overlay-hover-trigger clearfix"} )
+        for tag in tag_list:
+            if 'href' in tag.attrs:
+                stations.append(str(tag.attrs['href']))
+            
+        
+get_stations(area_url)
+
+
+def get_details(stations):
+    for detail in stations:
+        new_url2 = "http://tunein.com" + detail
+        html = urlopen(new_url2)
+        bsObj = BeautifulSoup(html, "html.parser")
+        info_list = bsObj.find( "div", {"class": 'info clearfix'}).findAll('h1')
+        for info in info_list:
+             name = info.get_text() 
+             print (info.get_text())
+        
+             
+        time.sleep(2)
+        followers = bsObj.find('li', {'class': 'followed-by fl-l'}).findAll( "div", {"class": 'count'})
+        for list in followers: 
+             follow = list.get_text()
+             print (list.get_text())
+        
+        time.sleep(2)
+             
+        
+        try: 
+            genres = bsObj.find('li', {'class': 'dark-link'}).findAll('a')
+            for type in genres: 
+                print (type.get_text())
+                music = type.get_text()
+            
+                
+        except:
+            print ( "None" )
+            music = 'None'
+            
+        
+        time.sleep(2)
+        location = bsObj.findAll('span', {'itemprop': 'contentLocation'})
+        for place in location:
+             where = place.get_text()
+             print (place.get_text())
+       
+        time.sleep(2)
+       
+        
+        radio_stations = [name, follow, music, where]
+        row = []
+            
+            
+        
+        for stuff in radio_stations: 
+                row.append(stuff)
+                
+        
+                
+    
+        c.writerow(row)    
+        time.sleep(1)
+        
+get_details(stations)
+csvfile.close()
+
+
+
